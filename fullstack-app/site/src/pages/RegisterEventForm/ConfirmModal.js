b86efaaa9a9fe392cad4b5ec02c27e56f5814fde
Jaime Huynh <taihuynh0901@gmail.com>
2020-09-18 03:03:01 -0700

    update style of the register page and fix bugs

--- fullstack-app/site/src/pages/RegisterEventForm/ConfirmModal.js
+++ fullstack-app/site/src/pages/RegisterEventForm/ConfirmModal.js
@@ -1,28 +1,22 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import { Modal } from "antd";
 
-const ConfirmModal = ({ show, modalData, handleClose }) => {
+const ConfirmModal = ({ visible, modalData, handleClose }) => {
   const { eventType, sourceName, versionName } = modalData;
   console.log(modalData);
 
   return (
     <>
-      <Modal show={show} hide={handleClose}>
-        <Modal.Header closeButton onClick={handleClose}>
-          <Modal.Title>Confirmation</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>You has successfully register a new event type!</p>
-          <p>Event Type Name: {eventType}</p>
-          <p>Source Name: {sourceName}</p>
-          <p>Version Number: {versionName}</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={handleClose}>
-            Submit another
-          </Button>
-        </Modal.Footer>
+      <Modal
+        title="Confirmation"
+        visible={visible}
+        onOk={handleClose}
+        onCancel={handleClose}
+      >
+        <p>You have successfully registered a new event type!</p>
+        <p>Event Type Name: {eventType}</p>
+        <p>Source Name: {sourceName}</p>
+        <p>Version Number: {versionName}</p>
       </Modal>
     </>
   );
--- fullstack-app/site/src/pages/RegisterEventForm/useDropdown.jsx
+++ fullstack-app/site/src/pages/RegisterEventForm/useDropdown.jsx
@@ -1,27 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const useDropdown = (label, defaultState, options, handleTypeSelectChange, idx ) => {
-    const [state, setState] = useState(defaultState);
-    const Dropdownmaker = () => (
-      <label htmlFor={label}>
-        {label}
-          <select
-            className="typeOfProp"
-            data-idx ={idx}
-            value={state}
-            onChange={e=>{
-              setState(e.target.value);
-              handleTypeSelectChange(e,idx,e.target.value)}}
-            onBlur={e=>setState(e.target.value)}
-            disabled={!options.length}
-          >
-            <option>Select</option>
-            {options.map(item=>
-            <option key={item} value={item}>{item}</option>)}
-          </select>
+const useDropdown = (
+  label,
+  defaultState,
+  options,
+  handleTypeSelectChange,
+  idx
+) => {
+  const [state, setState] = useState(defaultState);
+  const Dropdownmaker = () => (
+    <label htmlFor={label}>
+      {label}
+      <select
+        className="typeOfProp"
+        data-idx={idx}
+        value={state}
+        onChange={(e) => {
+          setState(e.target.value);
+          handleTypeSelectChange(e, idx, e.target.value);
+        }}
+        onBlur={(e) => setState(e.target.value)}
+        disabled={!options.length}
+      >
+        <option>Choose Prop Types</option>
+        {options.map((item) => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
     </label>
-    );
-    return [state, Dropdownmaker, setState]
-  }
+  );
+  return [state, Dropdownmaker, setState];
+};
 
-    export default useDropdown
+export default useDropdown;
